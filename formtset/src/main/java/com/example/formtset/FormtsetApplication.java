17e16245a5064aab6a5a6c1111118a3b2d4072a6
jini5 <lim789789@gmail.com>
2022-11-22 20:09:18 +0900

    study_221122

---
+++ formtset/src/main/java/com/example/formtset/FormtsetApplication.java
@@ -0,0 +1,13 @@
+package com.example.formtset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormtsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormtsetApplication.class, args);
+    }
+
+}
