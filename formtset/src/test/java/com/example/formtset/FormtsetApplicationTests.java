7deea786e9ccc543307313b8a9ed3c3ce1660b13
jini5 <lim789789@gmail.com>
2022-11-29 13:46:05 +0900

    221128

---
+++ formtset/src/test/java/com/example/formtset/FormtsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formtset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormtsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
