9ba29633b4cd5b2f87c92bc26f7e6b2274606314
jini5 <lim789789@gmail.com>
2022-12-13 12:52:40 +0900

    commit

---
+++ bitcoinTest/src/test/java/com/example/bitcointest/BitcoinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcointest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
