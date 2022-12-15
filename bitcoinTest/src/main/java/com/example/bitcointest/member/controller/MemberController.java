7b433f251e86b4fb33f505afa3889a4d9323fbd8
jini5 <lim789789@gmail.com>
2022-12-15 18:50:47 +0900

    member commit

---
+++ bitcoinTest/src/main/java/com/example/bitcointest/member/controller/MemberController.java
@@ -0,0 +1,62 @@
+package com.example.bitcointest.member.controller;
+
+import com.example.bitcointest.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class MemberController {
+
+    @Autowired
+    MemberService ms;
+
+    @PostMapping("/signup")
+    public String insertMember(@RequestParam HashMap<String,String> params){
+        ms.insertMember(params);
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginMember(@RequestParam HashMap<String,String> params, HttpSession session){
+        HashMap<String,Object> verified=ms.loginMember(params);
+        if(verified!=null) {
+            String email = String.valueOf(verified.get("email"));
+            session.setAttribute("email", email);
+
+            session.setAttribute("name", String.valueOf(verified.get("name")));
+            session.setAttribute("phone", String.valueOf(verified.get("phone")));
+            session.setAttribute("address", String.valueOf(verified.get("address")));
+            session.setAttribute("detail", String.valueOf(verified.get("detail")));
+        }
+        return "redirect:/";
+    }
+
+
+
+    @GetMapping("/hadBalance")
+    public @ResponseBody int hadBalance(HttpSession session){
+        if(session.getAttribute("email")!=null){
+            String email = String.valueOf(session.getAttribute("email"));
+            return ms.hadBalance(email);
+        }
+        return 0;
+    }
+
+    @GetMapping("/havingBalance")
+    public @ResponseBody ArrayList<HashMap<String, Object>> havingBalance(HttpSession session){
+        if(session.getAttribute("email")!=null){
+            String email = String.valueOf(session.getAttribute("email"));
+            return ms.havingBalance(email);
+        }
+        return null;
+    }
+}
