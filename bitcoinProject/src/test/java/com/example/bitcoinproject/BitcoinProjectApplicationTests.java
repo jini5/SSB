f86091f6786e8b304be857df318a03a588d093d8
jini5 <lim789789@gmail.com>
2022-12-19 13:12:48 +0900

    commit

---
+++ bitcoinProject/src/test/java/com/example/bitcoinproject/BitcoinProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.bitcoinproject;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
